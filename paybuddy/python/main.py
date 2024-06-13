0c25ef411a9ca678e85cf4ccf3db170d4cd07841
GhwFluffy <ghw@ghwfluffy.com>
2024-06-12 22:27:11 -0500

    Adding nginx configuration

--- paybuddy/python/main.py
+++ paybuddy/python/main.py
@@ -20,4 +20,4 @@ async def get_status(params: StatusRequestModel = Depends()):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
